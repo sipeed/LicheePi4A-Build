5162075519726877cc70056561d794d11b5725dc
Lu Hui <luhui@sipeed.com>
2023-12-28 09:39:47 +0800

    lock /boot/extlinux/extlinux.conf

--- overlay/opt/touch-setup.sh
+++ overlay/opt/touch-setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 rm /home/sipeed/.config/autostart/DisplaySetup.desktop
+# my work is toooo busy, haven't time merge some change into upstream
+# lock /boot/extlinux/extlinux.conf
+sudo apt purge u-boot-menu -y
 if (cat /proc/device-tree/model | grep LicheeConsole4A);
 then
 	sh /opt/console-setup.sh
